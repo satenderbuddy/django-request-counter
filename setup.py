9e655947b0175f3eab21a9f471e385e8518b8ffb
Satender Singh <satender.singh@careers360.in>
2024-01-04 15:51:42 +0530

    updated url and views according tho changes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 if __name__ == "__main__":
     setuptools.setup(
         name="request-counter",
-        version="1.2.0",
+        version="1.3.0",
         packages=[
             "request_counter",
             "request_counter/management/commands",
