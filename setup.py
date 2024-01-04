d4ec6e0bf7895402a54bb33696877f4a548f8bde
Satender Singh <satender.singh@careers360.in>
2024-01-04 12:33:19 +0530

    updated stable version

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ import setuptools
 
 if __name__ == "__main__":
     setuptools.setup(
-        name="request-counter", version="1.0.0", packages=["request_counter"]
+        name="request-counter", version="1.1.0", packages=["request_counter"]
     )
